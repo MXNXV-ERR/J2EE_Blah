69f2a6e3bfc0bf5c5f01a05338d06092f8f7c2d2
MXNXV-ERR <manavrai.jmn@gmail.com>
2023-02-28 21:37:50 +0530

    sort

--- Question_2/Question2/src/Book.java
+++ Question_2/Question2/src/Book.java
@@ -1,20 +1,16 @@
 public class Book {
-  
         String title;
         String author;
         String publisher;
         double price;
-        public Book(String x, String y, String z, double p) {
-        title=x;
-        author=y;
-        publisher=z;
-        price=p;
+        public Book(String title, String author, String publisher, double price) {
+                this.title = title;
+                this.author = author;
+                this.publisher = publisher;
+                this.price = price;
         }
         @Override
         public String toString() {
-            return "Book [title=" + title + ", author=" + author + ", publisher=" + publisher + ", price=" + price
-                    + "]";
+            return "Book [title=" + title + ", author=" + author + ", publisher=" + publisher + ", price=" + price + "]";
         }
-        
-        
 }
--- Question_2/Question2/src/BookCompare.java
+++ Question_2/Question2/src/BookCompare.java
@@ -1,10 +1,8 @@
 import java.util.Comparator;
 
 public class BookCompare implements Comparator<Book> {
-
     @Override
     public int compare(Book b1, Book b2) {
-        // TODO Auto-generated method stub
         if(b1.price>b2.price)
             return 1;
         else if(b1.price==b2.price) 
@@ -12,17 +10,4 @@ public class BookCompare implements Comparator<Book> {
         else
             return -1;
     }
-    // @Override
-    // public int compare(Book b1, Book b2) {
-    // // TODO Auto-generated method stub
-    // if(b1.price>b2.price)
-    // return 1;
-    // else if(b1.price==b2.price){
-    // return 0;
-    // }
-    // else
-    // {
-    // return -1;
-    // }
-
 }
